2a8a3343c6bc7d7c29aa35d2a6c9ddd1a9008459
JethyFlores <jethyflores3d@hotmail.com>
2026-03-11 19:36:09 -0500

    Cambiando modelo a gemini-3-pro-preview

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ app = FastAPI(title="Microservicio IA - AAAMundial")
 
 # Configuramos Gemini
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-3.1-flash-lite-preview')
 
 # 2. Bases de Datos Inmutables
 CATALOGO_MODULOS = {
